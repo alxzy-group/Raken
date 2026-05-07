a207615a0b0459d9b04b4e64e1616ec96ea286bd
alxzy-group <alansuper123tree@gmail.com>
2026-05-07 18:57:12 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,8 +1,10 @@
+require('dotenv').config();
+
 global.debug = false; 
 module.exports = {
-    PAKASIR_KEY: 'egY52Qm9D3WdeFpXoB9nipg5zFfhqDMC',
+    PAKASIR_KEY: process.env.PAKASIR_KEY || 'egY52Qm9D3WdeFpXoB9nipg5zFfhqDMC',
     PAKASIR_SLUG: 'ravenzena-auto-order',
-    GITHUB_TOKEN: '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',
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN, 
     GITHUB_OWNER: 'alxzy-group',
     GITHUB_REPO: 'alxzydb',
     FILE_PATH: 'orders.json',
@@ -17,8 +19,7 @@ module.exports = {
             'paket1': { nama: '1 Minggu', harga: 1000, hari: 7 },
             'paket2': { nama: '1 Bulan', harga: 10000, hari: 30 },
             'paket3': { nama: '2 Bulan', harga: 18000, hari: 60 }
-          
-         },
+        },
         guild: {
             'paket1': { nama: '1 Minggu', harga: 7000, hari: 7 },
             'paket2': { nama: '1 Bulan', harga: 15000, hari: 30 },
@@ -30,7 +31,6 @@ module.exports = {
             'paket1': { nama: '1 Minggu', harga: 6000, hari: 7 },
             'paket2': { nama: '1 Bulan', harga: 13000, hari: 30 },
             'paket3': { nama: '2 Bulan', harga: 21000, hari: 60 }
-           
         }
     }
 };
