a57f781364a1c9bfd508127ebac6634673731dbc
alxzy-group <alansuper123tree@gmail.com>
2026-05-07 22:05:11 +0700

    Update config.js

--- config.js
+++ config.js
@@ -21,7 +21,7 @@ module.exports = {
             'paket3': { nama: '2 Bulan', harga: 18000, hari: 60 }
         },
         guild: {
-            'paket1': { nama: '1 Minggu', harga: 7000, hari: 7 },
+            'paket1': { nama: '1 Minggu', harga: 1000, hari: 7 },
             'paket2': { nama: '1 Bulan', harga: 15000, hari: 30 },
             'paket3': { nama: '2 Bulan', harga: 23000, hari: 60 },
             'paket4': { nama: '3 Bulan', harga: 30000, hari: 90 },
